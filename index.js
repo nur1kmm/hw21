ef318b56c3738f3818c9315e4da69b4396d02ece
nur1kmm <148078275+nur1kmm@users.noreply.github.com>
2024-01-15 12:36:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+let square = (a) => a*a;
+
+
+const sumSquare = (b,c) => square(b) + square(c);
+const  sumSquare2 = (d,e) => d*d + e*e;
+
+
+function helloo(){
+  
+}
+
+const hello = (a) => {
+  let text = document.createElement("p");
+  text.textContent = a;
+  document.body.append(text);
+
+}
+
+hello("Hello, World"); 
